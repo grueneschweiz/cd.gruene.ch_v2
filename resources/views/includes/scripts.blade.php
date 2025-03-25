8014712828f11177e0f83aae2becb5f85bf2746f
Michael Schär <michael.schaer@gruene.ch>
2025-03-25 16:20:12 +0100

    Remove Crowdin Web Widget
    Crowdin Web Widget is discontinued and won't work after April 2 2025. Let's remove it.

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -1,12 +1,3 @@
-@if ( app('request')->input('translate') )
-    <script type="text/javascript">
-        var _jipt = [];
-        _jipt.push(['project', 'cdgruenech']);
-    </script>
-    <script type="text/javascript" src="//cdn.crowdin.com/jipt/jipt.js"></script>
-    <script>var crowdin_lang = 'zu';</script>
-@endif
-
 @if( Auth::user() && ! Auth::user()->pending_approval )
     <script src="{{ mix('js/app.js') }}" defer></script>
 @endif
